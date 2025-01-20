ecdbfb8e460fbe11440fdd976129cc2512543707
ValdirDEV143 <prof.valdir.escola@gmail.com>
2025-01-20 17:04:35 -0400

    task24

--- JS/script.js
+++ JS/script.js
@@ -9,7 +9,7 @@ function toggleMenu() {
     const nav = document.querySelector('.nav-responsive');
     menuHamburguer.classList.toggle('change');
 
-    if (menuHamburguer.classList.contains('Change')) {
+    if (menuHamburguer.classList.contains('change')) {
         nav.style.display = 'block'; 
     } else {
         nav.style.display = 'none';
